2c31fde8f311cf1c5a5a7e4916915a47ff8bdd6e
leelakin <fen@leelakin.com>
2015-10-19 23:20:44 +0100

    Pictures
    populated with pictures & additional changing comments box.

--- maffs_index.js
+++ maffs_index.js
@@ -5,79 +5,119 @@ $(".choicesec").on("click", ".headl", function(event){
 	var $content = $this.closest(".choicesec").find(".cont");
 
 	if(!$content.is(".activeSlide")){
-		//if($content.className != "activeSlide"){ ...didn't work.
-		//problem because 
 		$(".activeSlide").slideUp();
 		$(".activeSlide").removeClass("activeSlide");
 		$content.addClass("activeSlide");
-		//runAjaxCall($content);
+
+		if($content.hasClass("add")){
+			addRandomiser($content);
+		}else if($content.hasClass("sub")){
+			subRandomiser($content);
+		}else if($content.hasClass("mult")){
+			alert("not ready yet");
+		}else if($content.hasClass("div")){
+			divRandomiser();
+		}else if($content.hasClass("prime")){
+			alert("not ready yet");
+		};
+
 		$content.slideDown();
 
 	}else{
-		//could have alert 'Are you sure you want to close [section name]?'
 		$content.slideUp();
 		$(".activeSlide").removeClass("activeSlide");
 	};
-
-	if($content.hasClass("add")){
-		addRandomiser();
-	};
-
 });
 
-var addRandomiser = function(){
 
-	var x = Math.round((Math.random())*10);
-	var y = Math.round((Math.random())*10);
-	var addproblem = x+ " + " +y+ " = " + "<input type=\"text\" id=\"addfield\" placeholder=\"result\"> <input type=\"submit\" id=\"addsubmit\">";
+var addRandomiser = function($content){
+	$("#newadd").hide();
+	var x = Math.round((Math.random())*100); //not refreshing these when tab reopened
+	var y = Math.round((Math.random())*100); //just displays last state before slideUp
+	var addproblem = x+ " <b>+</b> " +y+ " = " + "<input type=\"text\" class=\"field\" id=\"addfield\" placeholder=\"result\"> <button type=\"submit\" id=\"addsubmit\">Enter</button>";
 	$("#addmaffs").html(addproblem);
 
-	$("#addsubmit").on("click", function(){
+	$("#addsubmit").on("click", function(event){
+		event.preventDefault;
 		var answer = $("#addfield").val();
-		if(isNaN(answer)){
-			alert("Please enter a valid number.");
+		var $bubble = $content.find(".bubble");
+		if(isNaN(answer) || answer ===""){
+			$bubble.html("Please enter a valid number! No letters or symbols.");
 		}else{
 			if(answer == (x+y)){
-				alert("Right!");
+				$content.find(".img").attr("src","rightimg2.png");
+				$bubble.html("Well done! The correct answer is " +(x+y)+ ". Click below to solve the next problem!");
+				$("#newadd").show();
 			}else{
-				alert("Wrong! Try again.");
+				$content.find(".img").attr("src","tryagimg2.png");
+				$bubble.html("Wrong answer! But don't worry, try again! [help icon]");
 			}
 		}
-	})
-	//when submit is clicked, get .val() & see if it's a number. alert.
-	//check whether number is the right result; alert.
-
-	$("#newadd").on("click", function(){
-		addRandomiser(); //is this a safe way to just refresh the inner HTML?
+	});
+	$("#newadd").on("click", function(event){
+		event.preventDefault;
+		addRandomiser($content); //is this a safe way to just refresh the inner HTML?
+		$(this).hide();
 	});
 };
 
+var subRandomiser = function($content){
+	var x = Math.round((Math.random())*100);
+	var y = Math.round((Math.random())*100);
+	while(y>x){
+		var y = Math.round((Math.random())*100);
+	};
+	var subproblem = x+ " - " +y+ " = " + "<input type=\"text\" class=\"field\" id=\"subfield\" placeholder=\"result\"> <button type=\"submit\" id=\"subsubmit\">Enter</button>";
+	$("#submaffs").html(subproblem);
 
+	$("#subsubmit").on("click", function(){
+		var answer = $("#subfield").val();
+		if(isNaN(answer) || answer ===""){
+			alert("Please enter a valid number.");
+		}else{
+			if(answer == (x-y)){
+				alert("Right!"); //change to HTML insert of Fox
+			}else{
+				alert("Wrong! Try again."); //change to HTML insert of help
+			}
+		}
+	});
 
-/*var runAjaxCall = function(sectionType){
+	$("#newsub").on("click", function(event){
+		event.preventDefault;
+		subRandomiser(); //is this a safe way to just refresh the inner HTML?
+	});
 
-	if(sectionType.hasClass("add")){
+};
 
-		$.ajax("maffs_addition.html", { //local works in Firefox, not Chrome.
-			//this url works in neither when all is uploaded, throws empty error
-			type: "GET",
-			dataType: "html",
-			success: function(response){
-				sectionType.append(response);
-			},
-			error: function(request,errorType,errorMsg){
-				alert("Error: "+errorType+" with message: "+errorMsg);
-			},
-			timeOut: 4000
-		})
-	}else if(sectionType.hasClass("sub")){
-		alert("sub");
-	}else if(sectionType.hasClass("mult")){
-		alert("mult");
-	}else if(sectionType.hasClass("div")){
-		alert("div");
-	}else if(sectionType.hasClass("prime")){
-		alert("prime");
+var divRandomiser = function(){
+	var x = Math.round((Math.random())*100);
+	var y = Math.round((Math.random())*100);
+	while(x%y!==0 || x==0 || y==0){
+		var x = Math.round((Math.random())*100);
+		var y = Math.round((Math.random())*100);
 	};
 
-};*/
+	var divproblem = x+ " / " +y+ " = " + "<input type=\"text\" class=\"field\" id=\"divfield\" placeholder=\"result\"> <button type=\"submit\" id=\"divsubmit\">Enter</button>";
+	$("#divmaffs").html(divproblem);
+
+	$("#divsubmit").on("click", function(event){
+		event.preventDefault;
+		var answer = $("#divfield").val()
+		if(isNaN(answer) || answer ===""){
+			alert("Please enter a valid number.");
+		}else{
+			if(answer == (x/y)){
+				alert("Right!"); //change to HTML insert of Fox
+			}else{
+				alert("Wrong! Try again."); //change to HTML insert of help
+			}
+		}		
+	})
+
+	$("#newdiv").on("click", function(event){
+		event.preventDefault;
+		divRandomiser(); //is this a safe way to just refresh the inner HTML?
+	});
+};
+
