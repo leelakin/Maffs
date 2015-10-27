28a34ba54c7eab9c1c3ebb563aa10adaae4ba378
leelakin <fen@leelakin.com>
2015-10-27 18:41:37 +0000

    Responsive styles & functional ajax calls added

--- maffs_index.js
+++ maffs_index.js
@@ -3,7 +3,7 @@ $(".choicesec").on("click", ".headl", function (event){
 	console.log("slider clicked, start functionality.");
 	event.preventDefault();
 	var $content = $(this).closest(".choicesec").find(".cont");
-
+	
 	if(!$content.is(".activeSlide")){//if the clicked slide isn't the active one
 		$(".activeSlide").slideUp();
 		$(".activeSlide").removeClass("activeSlide");
@@ -36,8 +36,11 @@ var problemBuilder = function($content){ //passing content will keep the context
 		return;
 	};
 
-	$bubble.html("Maths is fun when you get the hang of it! <br><img src=\"bulb.png\" class=\"bulb\"/> help")
-
+	if($content.hasClass("prime")){
+		$bubble.html("<b>Prime Numbers</b> are numbers which can only be divided by 1 and themselves!")
+	}else{
+		$bubble.html("Maths is fun when you get the hang of it! <br><img src=\"bulb.png\" class=\"bulb\"/> help")
+	};
 	//create problem based on class
 	var x = Math.round((Math.random())*100); 
 	var y = Math.round((Math.random())*100);
@@ -46,39 +49,35 @@ var problemBuilder = function($content){ //passing content will keep the context
 		correctAnswer = x+y; probSymbol = "+";
 	}else if($content.hasClass("sub")){
 		while(y>x){
-			var y = Math.round((Math.random())*100);
+			y = Math.round((Math.random())*100);
 		};
 		correctAnswer = x-y; probSymbol = "-";
 	}else if($content.hasClass("div")){
 		while(x%y!==0 || x==0 || y==0){
-			var x = Math.round((Math.random())*100);
-			var y = Math.round((Math.random())*100);
+			x = Math.round((Math.random())*100);
+			y = Math.round((Math.random())*100);
 		};
 		correctAnswer = x/y; probSymbol = "/";
 	}else if($content.hasClass("mult")){
-		var x = Math.round((Math.random())*10); 
-		var y = Math.round((Math.random())*10);
+		x = Math.round((Math.random())*10); 
+		y = Math.round((Math.random())*10);
 		correctAnswer = x*y; probSymbol = "*";
-	}else if($content.hasClass("prime")){
-		alert("prime triggered");
-		correctAnswer = "NOPE"; probSymbol = "NOPE";
 	}else{
-		alert("default triggered");
+		if(!$content.hasClass("prime")){
+			alert("default triggered");
+		};
 	};
 	//insert custom problem
-	var problem = x+ " <b>"+probSymbol+"</b> " +y+ " = " + "<input type=\"text\" class=\"field\" placeholder=\"result\"> <button type=\"submit\" class=\"submit\">Enter</button>";
-	$content.find(".problem").html(problem);
-
-	//HELP LISTENER
-	$(document).on("click", ".bulb", function(event){
-		event.preventDefault();
-		$("#help").show();
-		$(document).on("click");
-		$(document).on("click", "#closeX", function(event){
-			event.preventDefault();
-			$("#help").hide();
-		});
-	});
+	var problem;
+	if(!$content.hasClass("prime")){ //works
+		console.log("problem gets inserted for anything but prime");
+		problem = x+ " <b>"+probSymbol+"</b> " +y+ " = " + "<input type=\"text\" class=\"field\" placeholder=\"result\"> <button type=\"submit\" class=\"submit\">Enter</button>";
+		$content.find(".problem").html(problem);		
+	}else if ($content.hasClass("prime")){ //why doesn't this work??
+		console.log("class was prime, insert problem");
+		problem = "<input type=\"text\" class=\"field\" placeholder=\"result\"> <button type=\"submit\" class=\"submit\">Enter</button>";
+		$content.find(".problem").html(problem);
+	};
 
 	//SUBMIT LISTENER
 	$(".submit").on("click", function(event){
@@ -108,3 +107,37 @@ var problemBuilder = function($content){ //passing content will keep the context
 		console.log("answer: " +answer+ ", correctAnswer: " +correctAnswer);
 	});
 };
+
+
+//HELP LISTENER
+$(document).on("click", ".bulb", function (event){
+	event.preventDefault();
+	var $content = $(event.target).closest(".choicesec").find(".cont");
+	console.log("bulb clicked. $content classes: " +$content.attr("class"));
+
+	$("#help").show();
+	
+	var helpClass = $content.attr("class").split(" ");
+
+	console.log("split array method shows class " +helpClass[0]);
+	//build custom ajax request
+	$.ajax({
+		url: "ajax_" +helpClass[0]+ ".html",
+		success: function(result){
+			$("#helptext").html(result);
+		},
+		error: function(request, errorType, errorMsg){
+			$("#helptext").text("Error! " +errorType+ " with message " +errorMsg);
+		},
+		timeOut: 4000,
+		beforesend: function(){
+			$("#helptext").text("Please wait, help loading...")
+		}
+	});
+
+
+	$(document).on("click", "#closeX", function(event){
+		event.preventDefault();
+		$("#help").hide();
+	});
+});
